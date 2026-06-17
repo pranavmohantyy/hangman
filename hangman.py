7cb3ad930954be10dae3dc3a8cb4dd527afef024
pranavmohantyy <mohantypranav147@gmail.com>
2026-06-17 14:26:02 -0700

    move word bank to json

--- hangman.py
+++ hangman.py
@@ -1,8 +1,10 @@
 import random
+import json
 
 class Hangman:
     def __init__(self):
-        self.word_list = ['python', 'hangman', 'programming', 'developer', 'github']
+        with open('words.json') as f:
+            self.word_bank = json.load(f)
         self.max_failures = 6
         self.stages = [
             "\n\n\n\n\n\n\n",
@@ -10,41 +12,11 @@ class Hangman:
             "______\n|    |\n|    O\n|   /|\n|   /\n|\n",
             "______\n|    |\n|    O\n|    |\n|   / \\n|\n",
             "______\n|    |\n|    O\n|    |\n|   /\n|\n",
-            "______\n|    |\n|    O\n|\n|\n|\n",
+            "______\n|    |\n|    O\n|    |\n|\n|\n",
             "______\n|    |\n|\n|\n|\n|\n"
         ]
 
-    def choose_word(self):
-        return random.choice(self.word_list)
+    def choose_word(self, category):
+        return random.choice(self.word_bank[category])
 
-    def play(self):
-        word = self.choose_word()
-        guessed = ['_'] * len(word)
-        failures = 0
-        guessed_letters = []
-
-        while failures < self.max_failures and '_' in guessed:
-            print(self.stages[failures])
-            print(' '.join(guessed))
-            guess = input('Guess a letter: ').lower()
-
-            if guess in guessed_letters:
-                print('You already guessed that letter.')
-                continue
-            if guess in word:
-                for index, letter in enumerate(word):
-                    if letter == guess:
-                        guessed[index] = guess
-            else:
-                failures += 1
-            guessed_letters.append(guess)
-
-        if '_' not in guessed:
-            print('Congratulations! You guessed the word:', word)
-        else:
-            print(self.stages[failures])
-            print('Game over! The word was:', word)
-
-if __name__ == '__main__':
-    game = Hangman()
-    game.play()
+    # Other methods will go here...
