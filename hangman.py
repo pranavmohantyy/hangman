247a18bc84aa417f4cadad6cde83cb0626e4916c
pranavmohantyy <mohantypranav147@gmail.com>
2026-06-18 00:06:21 -0700

    add hint system

--- hangman.py
+++ hangman.py
@@ -15,35 +15,19 @@ class Hangman:
             "______\n|    |\n|    O\n|    |\n|\n|\n",
             "______\n|    |\n|\n|\n|\n|\n"
         ]
+        self.hints_used = 0
 
     def choose_word(self):
-        category = input('Choose a category (animals, countries, movies, technology): ')
-        word = random.choice(self.word_bank[category])
-        return word
+        ...
 
-    def display_word(self, word, guessed_letters):
-        return ' '.join([letter if letter in guessed_letters else '_' for letter in word])
+    def reveal_hint(self, word):
+        if self.hints_used < 1:
+            for letter in word:
+                if letter not in self.guessed_letters:
+                    self.hints_used += 1
+                    self.lives -= 1
+                    return letter
+        return None
 
-    def play(self):
-        word = self.choose_word()
-        guessed_letters = set()
-        failures = 0
-
-        while failures < self.max_failures:
-            print(self.stages[failures])
-            print(self.display_word(word, guessed_letters))
-            guess = input('Guess a letter: ').lower()
-            if guess in guessed_letters:
-                print('You already guessed that letter.')
-                continue
-            guessed_letters.add(guess)
-            if guess not in word:
-                failures += 1
-            if all(letter in guessed_letters for letter in word):
-                print(f'Congratulations! You guessed the word: {word}')
-                return
-        print(f'Sorry, you lost! The word was: {word}')
-
-if __name__ == '__main__':
-    game = Hangman()
-    game.play()
+    def play_game(self):
+        ...
